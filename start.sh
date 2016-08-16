b6e7737b8ef6dfc6a02034210a94be781b53bc98
root <root@localhost.localdomain>
2016-08-16 04:44:21 -0400

    add start.sh

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+DJANGODIR=/home/endless-handsome
+#DJANGO_SETTINGS_MODULE=app.settings.prod
+
+LOGFILE=/var/log/gunicorn/guni-app.log
+LOGDIR=$(dirname $LOGFILE)
+NUM_WORKERS=2
+
+# user/group to run as
+#USER=ubuntu
+#GROUP=ubuntu
+
+cd /home/endless-handsome
+source /home/venv/bin/activate
+
+#export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+test -d $LOGDIR || mkdir -p $LOGDIR
+exec gunicorn endlesshandsome.wsgi:application -w $NUM_WORKERS \
+  #--user=$USER --group=$GROUP 
+  --log-level=debug \
+  --log-file=$LOGFILE -b 0.0.0.0:8000 2>>$LOGFILE
