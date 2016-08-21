fe864be057b9f5d5fee30a3631da9be5f4f8ac19
胡义晋 <jingdiqkl123@qq.com>
2016-08-21 11:24:54 +0800

    website index

---
+++ website/static/website/js/main.js
@@ -0,0 +1,33 @@
+$(function(){
+	var fullContainer = $(".full-container");
+	var navBtns = fullContainer.find(".nav-2l a");
+
+	var SectionHandler ={
+		currentSection:null,
+		sections:{},
+		init:function(){
+			SectionHandler.gotoSection("section_1");
+			navBtns.on("click",function(e) {
+				SectionHandler.gotoSection($(e.target).data("section"));
+			});
+		},
+		gotoSection:function(id){
+			var section = SectionHandler.sections[id]?SectionHandler.sections[id]:SectionHandler.sections[id]=$("#"+id);
+			if(SectionHandler.currentSection){
+				SectionHandler.fadeOut(SectionHandler.currentSection);
+			}
+			SectionHandler.fadeIn(section);
+			SectionHandler.currentSection = section;
+		},
+		fadeOut:function(section){
+			section.css("top","110%");
+		},
+		fadeIn:function(section){
+			section.css("top","10%");
+		}
+	}
+
+
+	SectionHandler.init();
+	
+});
